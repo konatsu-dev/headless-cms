6836d09a6f2039b44a92d986dba9330920f14b38
Konatsu <konatsu@sonosoft.com>
2025-07-16 14:54:27 +0900

    [fix] サムネイル画像表示

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -4,6 +4,9 @@ type Post = {
   title: {
     rendered: string;
   };
+  _embedded?: {
+    [key: string]: any;
+  };
 };
 
 async function getPosts(): Promise<Post[]> {
@@ -25,9 +28,17 @@ export default async function Home() {
     <main style={{ padding: '2rem' }}>
       <h1>WordPress記事一覧</h1>
       <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title.rendered}</li>
-        ))}
+        {posts.map((post) => {
+          const thumbnail = post._embedded?.["wp:featuredmedia"]?.[0]?.source_url;
+          return (
+            <li key={post.id} style={{ marginBottom: '2rem' }}>
+              {thumbnail && (
+                <img src={thumbnail} alt={post.title.rendered} style={{ maxWidth: 200, display: 'block' }} />
+              )}
+              {post.title.rendered}
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
